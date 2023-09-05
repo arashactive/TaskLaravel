eda3be3f5736b4d758035b899311f34d41fd33c2
Arash Dehghani <arash.aspx@gmail.com>
2023-09-05 19:15:44 +0330

    Services structre done

--- app/Http/Controllers/web/financial/XmlController.php
+++ app/Http/Controllers/web/financial/XmlController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Api\V1;
+namespace App\Http\Controllers\web\financial;
 
 use App\Http\Controllers\Controller;
 use App\Http\Traits\JsonRespondController;
--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,10 @@ use App\Http\Controllers\Api\V1\RefundController;
 use App\Http\Controllers\Api\V1\XmlController;
 
 Route::post('customer/upload', [RefundController::class, 'uploadCustomers']);
-
 Route::get('xmls', [XmlController::class, 'listXML']);
 Route::get('xml/{id}/download', [XmlController::class, 'download']);
 Route::get('xml/{id}', [XmlController::class, 'xmlById']);
 Route::get('xml/{id}/log', [XmlController::class, 'logById']);
 
 
-Route::get('/', function () {
-    return view('web.sections.static.login');
-});
+Route::get('/login', '');
