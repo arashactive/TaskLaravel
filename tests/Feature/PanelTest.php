72e65dac00627cc73a5693e64b6325b76f9e5de2
Arash Dehghani <arash.aspx@gmail.com>
2023-09-06 23:38:30 +0330

    panelTest

--- tests/Feature/PanelTest.php
+++ tests/Feature/PanelTest.php
@@ -9,10 +9,34 @@ use Tests\TestCase;
 class PanelTest extends TestCase
 {
     
-    public function test_example()
+    public function test_page_login()
     {
         $response = $this->get('/');
+        $response->assertStatus(200);
+    }
 
+    public function test_page_dashboard()
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+
+    public function test_page_company()
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+    
+    public function test_page_excel_init()
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+
+
+    public function test_page_xml_by_id()
+    {
+        $response = $this->get('/');
         $response->assertStatus(200);
     }
 }
