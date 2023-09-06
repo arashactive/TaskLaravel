e3b1d2e394d9d8a00ac3271624004d1f7b46ff2f
Arash Dehghani <arash.aspx@gmail.com>
2023-09-06 23:35:25 +0330

    test start

---
+++ tests/Feature/PanelTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PanelTest extends TestCase
+{
+    
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
