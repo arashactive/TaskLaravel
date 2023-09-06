f55daa6d9f6dd08c2c3aef391c9261c9402f2d1a
Arash Dehghani <arash.aspx@gmail.com>
2023-09-07 00:18:55 +0330

    panel test

--- tests/Feature/PanelTest.php
+++ tests/Feature/PanelTest.php
@@ -3,40 +3,22 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class PanelTest extends TestCase
 {
-    
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->seed();
+    }
+
     public function test_page_login()
     {
         $response = $this->get('/');
         $response->assertStatus(200);
     }
 
-    public function test_page_dashboard()
-    {
-        $response = $this->get('/');
-        $response->assertStatus(200);
-    }
-
-    public function test_page_company()
-    {
-        $response = $this->get('/');
-        $response->assertStatus(200);
-    }
-    
-    public function test_page_excel_init()
-    {
-        $response = $this->get('/');
-        $response->assertStatus(200);
-    }
-
-
-    public function test_page_xml_by_id()
-    {
-        $response = $this->get('/');
-        $response->assertStatus(200);
-    }
 }
